b2d1b557e36768910c32de48380956a0e6a05328
JakeOh <jake.oh.lecture@gmail.com>
2018-05-02 16:57:53 +0900

    add module chapter 2

--- chapter02/src/main/kotlin/edu/programming/kotlin/ch02/ProgramCh02.kt
+++ chapter02/src/main/kotlin/edu/programming/kotlin/ch02/ProgramCh02.kt
@@ -2,4 +2,59 @@ package edu.programming.kotlin.ch02
 
 fun main(args: Array<String>) {
     println("Chapter 2. Kotlin Basics")
+
+    // Vals and vars
+
+    // Type inference
+
+    // Basic types
+    // Numbers
+    // Booleans
+    // Chars
+
+    // Strings
+
+    // Arrays
+
+    // Comments
+
+    // Packages
+
+    // Imports
+    // Wildcard imports
+    // Import renaming
+
+    // String templates
+
+    // Ranges
+
+    // Loops
+
+    // Exception handling
+
+    // Intantiating classes
+    // Referential equality and structural equality
+    // This expression
+
+    // Scope
+    // Visibility modifiers
+    // Private
+    // Protected
+    // Internal
+
+    // Control flow as expressions
+
+    // Null syntax
+
+    // Smart casts
+    // Explicit casting
+
+    // When expression
+    // When (value)
+    // When without argument
+
+    // Function Return
+
+    // Type hierarchy
+
 }
